28eb7d752375468b85140b76ac3543f2726b7507
kanakeswar-2000 <pkanakeswarareddy@gmail.com>
2025-09-29 23:55:50 +0530

    slicing the list

--- Lists/First_Last_elements.py
+++ Lists/First_Last_elements.py
@@ -1,5 +1,6 @@
 '''
 Given an integer N , write a program to read N inputs and form a list with first and last two elements
+It is assumed that N is greater than or equal to 4
 '''
 
 N=int(input())
